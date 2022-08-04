01ddfd53eb21459227f743f82224a7ab6e6b24bc
jpritha <aripritha1998@gmail.com>
2022-08-04 21:43:13 +0530

    second commit

---
+++ Leetcode/Mirror Reflection.cpp
@@ -0,0 +1,12 @@
+// Link : https://leetcode.com/problems/mirror-reflection/
+
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0){ 
+            p/=2;
+            q/=2;
+        }
+        return 1 - p % 2 + q % 2;
+    }
+};
