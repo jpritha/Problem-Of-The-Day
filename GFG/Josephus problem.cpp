a0c4accdc295ed66a71fa308e41ea9704107a091
jpritha <aripritha1998@gmail.com>
2022-08-09 00:32:30 +0530

    Please commit

---
+++ GFG/Josephus problem.cpp
@@ -0,0 +1,16 @@
+// Link : https://practice.geeksforgeeks.org/problems/josephus-problem/1
+
+
+class Solution
+{
+    public:
+    int solve(int n, int k) {
+        if(n == 1) return 0;
+        
+        return (solve(n-1, k)+k)%n;
+    }
+    int josephus(int n, int k)
+    {
+       return solve(n,k)+1;
+    }
+};
